f8413ae18291df891c865537fc08b80aa8214ade
Sri Vidya <svvilipala@gmail.com>
2024-10-28 20:42:52 -0500

    Updated mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET mydata page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sri Vidya Vilipala' });
+  res.render('mydata', { title: 'Sri Vidya Vilipala' });
 });
 
 module.exports = router;
