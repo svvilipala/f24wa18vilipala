eec03709d46070080a470c0d58a998c8564a91e5
Sri Vidya <svvilipala@gmail.com>
2024-10-30 17:32:14 -0500

    Added computation files

--- routes/computation.js
+++ routes/computation.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 let requestCount = 0; // To keep track of the request number
-
 // Helper function to apply a Math function in rotation
 function applyMathFunction(x) {
 
@@ -15,21 +14,15 @@ function applyMathFunction(x) {
   const value3 = `${x} is applied to floor and the result is ${floorResult}`;
 
   return `${value1}<br>${value2}<br>${value3}`;
-  
-
 }
-
 // Computation endpoint
 router.get('/', (req, res) => {
   const x = req.query.x ? parseFloat(req.query.x) : Math.random() * 100;
-
   if (isNaN(x)) {
     return res.status(400).send("Invalid value for x. Please provide a valid number.");
   }
-
-
   const result = applyMathFunction(x);
   res.send(result);
 });
-  
+
 module.exports = router;
