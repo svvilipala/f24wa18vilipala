e04a8e4cf9f007d9e75053abd8d76f176acff809
Sri Vidya <svvilipala@gmail.com>
2024-10-30 12:06:14 -0500

    updated users.js

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ var router = express.Router();
 let value = 0;
 let total = 1;
 router.get('/', function(req, res,next) {
-  value +=2;
+  value = value + 2;
   total = total + value;
   res.send('Total is: ' + total);
 });
